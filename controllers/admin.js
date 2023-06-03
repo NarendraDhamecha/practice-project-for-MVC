fe4fc2604d718d60ef433914405bf173aa35daf6
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-06-03 19:38:00 +0530

    dynamic routes part_3 (query params)

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -15,17 +13,61 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedtitle = req.body.title;
+  const updatedimageUrl = req.body.imageUrl;
+  const updatedprice = req.body.price;
+  const updateddescription = req.body.description;
+
+  const product = new Product(
+    prodId,
+    updatedtitle,
+    updatedimageUrl,
+    updateddescription,
+    updatedprice
+  );
+  product.save()
+  res.redirect('/admin/products');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
     });
   });
 };
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.deleteProductById(prodId);
+  res.redirect('/admin/products');
+}
--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,23 +27,41 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex((p) => p.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteProductById(id){
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      const filteredProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(filteredProducts), (err) => {
         console.log(err);
       });
-    });
+    })
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
 
-  static findById(id, cb){
-    getProductsFromFile(products => {
-     const product = products.find(p => p.id == id);
-     cb(product);
-    })
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id == id);
+      cb(product);
+    });
   }
 };
